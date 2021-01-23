9e84f72c0c79c787dd14ac6c5e1ac71895415493
K4STR0 <rrrcccsss777@gmail.com>
2021-01-23 14:29:17 +0100

    sql update

--- casting3.sql
+++ casting3.sql
@@ -14,6 +14,12 @@ create table nombre_cliente(
 	apellido2 char(20)
 );
 
+create table telefono_cliente(
+	cod_cliente char(20),
+	telefono int,
+	foreign key(cod_cliente) references cliente
+)
+
 create table casting(
 	cod_casting char(20),
 		primary key(cod_casting),
@@ -27,4 +33,41 @@ create table casting_cliente(
 	cod_cliente char(20),
 	foreign key(cod_casting) references casting,
 	foreign key(cod_cliente) references cliente
+);
+
+create table casting_online(
+	cod_casting char(20),
+	num_personas int,
+	fecha date,
+	web char(40),
+	foreign key(cod_casting) references casting
+);
+
+create table casting_presencial(
+	cod_casting char(20),
+	num_personas int,
+	fecha date,
+	cod_empleado char(20),
+	foreign key(cod_casting) references casting,
+	foreign key(cod_empleado) references empleado
+);
+
+create table candidato(
+	cod_candidato char(20),
+	direccion char(40),
+	telefono int,
+	nacimiento date,
+	importe int,
+	dni_representante char(40),
+	cod_perfil char(20),
+	foreign key(dni_representante) references representante,
+	foreign key(cod_perfil) references perfil
+);
+
+create table nombre_candidato(
+	cod_candidato char(20),
+	nombre char(20),
+	apellido1 char(20),
+	apellido2 char(20),
+	foreign key(cod_candidato) references candidato
 );
