55755aeea99dd2e90fcb476e977e96e5dffa8b52
K4STR0 <rrrcccsss777@gmail.com>
2021-01-24 16:16:39 +0100

    sql update

--- casting3.sql
+++ casting3.sql
@@ -1,3 +1,11 @@
+create domain Euros numeric(12,2);
+create domain Provincias char(20)
+	constraint comprobacion_provincia
+		check(value in('Albacete','Alicante','Almería','Álava','Asturias','Ávila','Badajoz','Islas Baleares','Barcelona','Bizkaia','Burgos','Cáceres','Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba','A Coruña','Cuenca','Gipuzkoa','Girona','Granada','Guadalajara','Huelva','Huesca','Jaén','León','Lleida','Lugo','Madrid','Málaga','Murcia','Navarra','Ourense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Santa Cruz de Tenerife','Segovia','Sevilla','Soria','Tarragona','Teruel','Toledo','Valencia','Valladolid','Zamora','Zaragoza','Ceuta','Melilla'));
+create domain Telefono numeric(9);
+
+
+
 create table cliente(
 	cod_cliente char(20),
 		primary key(cod_cliente),
@@ -16,9 +24,17 @@ create table nombre_cliente(
 
 create table telefono_cliente(
 	cod_cliente char(20),
-	telefono int,
+	telefono Telefono,
 	foreign key(cod_cliente) references cliente
-)
+);
+
+create table agente_casting(
+	cod_empleado char(20),
+	dni char(40),
+	nombre char(20),
+	direccion char(40),
+	primary key(cod_empleado)
+);
 
 create table casting(
 	cod_casting char(20),
@@ -49,17 +65,38 @@ create table casting_presencial(
 	fecha date,
 	cod_empleado char(20),
 	foreign key(cod_casting) references casting,
-	foreign key(cod_empleado) references empleado
+	foreign key(cod_empleado) references agente_casting
+);
+
+create table representante(
+	dni char(40),
+	nombre char(20),
+	direccion char(40),
+	telefono Telefono,
+	primary key(dni)
+);
+	
+create table perfil(
+	cod_perfil char(20),
+	provincia Provincias,
+	sexo char (1) check(sexo in('M','F')),
+	rango_edad char(10),
+	rango_altura char(10),
+	color_pelo char(10),
+	color_ojos char(10),
+	especialidad char(1) check(especialidad in('A','M')),
+	experiencia boolean,
+	primary key(cod_perfil)
 );
 
 create table candidato(
 	cod_candidato char(20),
 	direccion char(40),
-	telefono int,
 	nacimiento date,
-	importe int,
+	importe Euros,
 	dni_representante char(40),
 	cod_perfil char(20),
+	primary key(cod_candidato),
 	foreign key(dni_representante) references representante,
 	foreign key(cod_perfil) references perfil
 );
@@ -71,3 +108,83 @@ create table nombre_candidato(
 	apellido2 char(20),
 	foreign key(cod_candidato) references candidato
 );
+
+create table telefono_candidato(
+	cod_candidato char(20),
+	telefono Telefono,
+	foreign key(cod_candidato) references candidato
+);
+
+create table adulto(
+	cod_candidato char(20),
+	dni char(40),
+	foreign key(cod_candidato) references candidato
+);
+	
+create table niño(
+	cod_candidato char(20),
+	nombre_tutor char(20),
+	foreign key(cod_candidato) references candidato
+);
+
+create table prueba(
+	cod_prueba char(20),
+	sala char(20),
+	descripcion char(40),
+	precio Euros,
+	primary key(cod_prueba)
+);
+
+
+create table fase(
+	cod_fase char(20),
+	cod_casting char(20),
+	primary key(cod_fase),
+	foreign key(cod_casting) references casting
+);
+
+create table fase_prueba(
+	cod_fase char(20),
+	cod_prueba char(20),
+	foreign key(cod_fase) references fase,
+	foreign key(cod_prueba) references prueba
+);
+
+create table resultado_prueba(
+	cod_prueba char(20),
+	cod_candidato char(20),
+	superada boolean,
+	foreign key(cod_prueba) references prueba,
+	foreign key(cod_candidato) references candidato
+);
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
