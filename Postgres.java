2df7eb4d587c1f8893acedaf4505c760a36213dc
jmanufa01 <75038141+jmanufa01@users.noreply.github.com>
2021-01-27 19:09:33 +0100

    Postgres.java

---
+++ Postgres.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject4;
+
+import java.sql.*;
+import java.util.Scanner;
+
+/**
+ * 
+ * 
+ * @author Manu
+ */
+public class Postgres {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static String usuario;
+    private static String contraseña;
+    private static String nombreBaseDatos;
+    
+    
+    public static void main(String[] args) {
+        // FASES UNO  cargar el controlador de postgres
+        System.out.println("probando si funciona el driver postgres");
+        Scanner scan= new Scanner(System.in);
+        try{
+            Class.forName("org.postgresql.Driver");
+                    }
+        catch(ClassNotFoundException cnfe) {
+            System.out.println("driver no disponible");
+            System.out.println("error y salida");
+            cnfe.printStackTrace();
+            System.exit(1);
+        }
+       System.out.println("Driver cargado....empieza la conexiÃ³n"); 
+       // FASE DOS probar el manejador con su conexiÃ³n (ojo cambiar usuario y password)
+       Connection c = null;
+       
+       try {
+              System.out.println("Introduzca el nombre de la base de datos: ");
+              nombreBaseDatos = scan.nextLine();
+              System.out.println("Introduzca el nombre de usuario: ");
+              usuario = scan.nextLine();
+              System.out.println("Introduzca la contraseña: ");
+              contraseña = scan.nextLine();
+              
+              
+              c = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/"+nombreBaseDatos,
+                   usuario, contraseña);
+       }
+       catch(SQLException se) {
+                  System.out.println("No se pudo establecer la conexiÃ³n"); 
+                  se.printStackTrace();
+                  System.exit(1);
+       }
+       if ( c!= null)
+           System.out.println("Conectados a la base de datos");
+       else
+           System.out.println("Problemas al conectarnos a la base de datos"); 
+       // FASE TRES probando la generaciÃ³n de sentencias
+       Statement s = null;
+       try {
+            s = c.createStatement();
+       }
+        catch(SQLException se) {
+                  System.out.println("probando conexion de consulta"); 
+                  se.printStackTrace();
+                  System.exit(1);
+       }
+       // FASE IV probando la ejecuciÃ³n
+    ResultSet rs = null;
+    try {
+        String operacion;
+        System.out.println("Introduzca la operación que desea realizar en la base de datos: ");
+        operacion = scan.nextLine();
+        rs = s.executeQuery(operacion);
+    }
+    catch(SQLException se) {
+                  System.out.println("Comprobando consulta de select"); 
+                  se.printStackTrace();
+                  System.exit(1);
+    }
+    // FASE V viendo el resultado
+    int index = 0;
+    try {
+        while (rs.next()) {
+            System.out.println("resultado fila" + index++ +" "
+                    + rs.getString(1) + " " + rs.getString(2) ); 
+        }
+    }
+    catch(SQLException se) {
+                  System.out.println("Error grave al mostrar datos"); 
+                  se.printStackTrace();
+                  System.exit(1);
+    }
+    }
+}
