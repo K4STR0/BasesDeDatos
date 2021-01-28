778e27e20674ae090c991d6ef25119cf402f3722
jmanufa01 <75038141+jmanufa01@users.noreply.github.com>
2021-01-28 21:15:22 +0100

    triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,13 +1,34 @@
+
+
+
+
+
+
+
+
+
 create function contrata() returns trigger
 as
 $$
-declare
-codigo_casting char(20) := (select prueba.cod_casting 
-	   from prueba 
-	   where prueba.cod_prueba = new.cod_prueba);
+
+
+declare total_pruebas_candidato integer := (select count(cod_candidato) from resultado_pruebas 
+											where cod_candidato = new.cod_candidato and cod_prueba 
+												in(select cod_prueba from fase_prueba inner join fase on fase_prueba.cod_fase=fase.cod_fase
+												   where cod_prueba = new.cod_prueba));
+												   
+declare total_pruebas_casting integer := (select count(cod_casting) from fase_prueba inner join fase on fase_prueba.cod_fase = fase.cod_fase
+											where cod_fase in(select cod_fase from fase_prueba where cod_prueba=new.cod_prueba));
+
+
+declare codigo_casting varchar(20) := (select cod_casting from fase inner join fase_prueba on fase.cod_fase = fase_prueba.cod_fase 
+											where fase_prueba.cod_prueba = new.cod_prueba);
+
 begin
-insert into contrata(cod_cliente,codigo_casting)
-values(new.cod_candidato, codigo_casting);
+
+
+	if total_pruebas_candidato = total_pruebas casting then insert into contrata values(codigo_casting,new.cod_candidato);
+	end if;
 
 return new;
 end
@@ -18,7 +39,7 @@ Language plpgsql;
 
 create trigger importe_total after insert on resultado_prueba
 for each row
-execute procedure contrata();	
+execute procedure contrata();		
 
 
 
@@ -48,3 +69,28 @@ Language plpgsql;
 create trigger seleccion before insert on contrata for each row
 execute procedure seleccionados();
 
+
+
+
+
+
+
+create function ver_perfil() returns trigger
+as
+$$
+
+declare perfil varchar(10):= (select cod_perfil from candidato where cod_candidato = new.cod_candidato);
+
+begin
+	
+	if (perfil in (select cod_perfil from casting_perfil))
+		then insert into resultado_prueba values(new.cod_candidato,new.cod_prueba,new.superada);
+	end if;
+return new;
+end
+$$
+Language plpgsql;
+
+
+create trigger perfil_requerido before insert on resultado_prueba for each row
+execute procedure ver_perfil();
